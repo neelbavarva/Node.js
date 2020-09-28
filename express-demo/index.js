4b24931c50e57c0102dbdaa6bfba3f9bab137a45
Neel Bavarva <neelbavarva5@gmail.com>
2020-09-29 04:38:40 +0530

    routes are not working

--- express-demo/index.js
+++ express-demo/index.js
@@ -3,8 +3,14 @@ const app=express();
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
-app.get('/api/courses',(req,res)=>{
-    res.send([1,2,3]);
-});
+// app.get('/api/courses',(req,res)=>{
+//     res.send([1,2,3]);
+// });
 
-app.listen(3000, ()=> console.log('Listening on port 3000...'));
+app.get('/api/courses/:id',(req,res) => {
+    res.send(req.params.id);
+})
+
+const port = process.env.port || 3000 ;
+
+app.listen(port, ()=> console.log('Listening on port 3000...'));
