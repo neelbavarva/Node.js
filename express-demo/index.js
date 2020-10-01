998c2623c7cb695b3ca1ee219ef8808b59eb1e8b
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-01 13:14:51 +0530

    ..

--- express-demo/index.js
+++ express-demo/index.js
@@ -65,4 +65,4 @@ app.get('/api/courses/:id',(req,res) => {
 
 const port = process.env.port || 3000 ;
 
-app.listen(port, ()=> console.log('Listening on port 3000...'));
+app.listen(port, ()=> console.log('Listening on port 3000.....'));
