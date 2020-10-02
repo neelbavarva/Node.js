3b516f2c6c35ecc7c4f39672eea977883295083e
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-02 18:09:23 +0530

    mongoose

---
+++ mongo-demo/index.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(()=>console.log("Connected to mongoDB..."))
+    .catch(err => console.error("Could not connect to MongoDB..",err))
