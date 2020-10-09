82151dcae8a96c27a4e894bdfd331a878d0a0b6d
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-09 19:08:08 +0530

    ...jjj

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -2,4 +2,4 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
     .then(()=>console.log("Connected to mongoDB..."))
-    .catch(err => console.error("Could not connect to MongoDB..",err))
+    .catch(err => console.error("Could not connect to MongoDB...",err))
