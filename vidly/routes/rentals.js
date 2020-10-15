e89c7eaa5acc1f89564dcf4a50ff0d89881496a1
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-15 17:32:57 +0530

    ZIP file not available

---
+++ vidly/routes/rentals.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+const {Rental, validate} = require("../models/rental");
+const {Movie} = require('../models/movie');
+const {Customer} = require('../models/customer');
+
+router.get('/', async(req, res) => {
+    const rentals = await Rental.find().sort('-dateOut');
+    res.send(rentals);
+});
+
+router.post('/', async(req, res) => {
+    const {error} =validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findById(req.body.customerId);
+    if(!customer) return res.status(400).send('Invalid customer');
+
+    const movie = await Movie.findById(req.body.movieId);
+    if(!movie) return res.status(400).send('Invalid movie');
+
+    if(movie.numberInStock === 0) return res.status(400).send('Movie not in stock');
+
+    let rental = new Rental({
+        customer: {
+            _id: customer._id,
+            name: customer.name,
+            phone: customer.phone
+        },
+    })
+
+})
